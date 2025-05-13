ab53efeff073c16cd58faf8c32d30079b77db0b8
firman <rus.guro123@gmail.com>
2025-05-13 21:31:18 +0300

    Слияние со вторым тестом

--- winFormsApp2.Tests/UnitTests.cs
+++ winFormsApp2.Tests/UnitTests.cs
@@ -8,7 +8,6 @@ namespace winFormsApp2.Tests
         [Fact]
         public void LoginBTN_Click_EmptyLogin_ShowsErrorMessage()
         {
-            // Arrange
             LoginForm form = new LoginForm();
             form.textBox1 = new TextBox();
             form.textBox2 = new TextBox();
@@ -24,10 +23,30 @@ namespace winFormsApp2.Tests
                 messageBoxText = text;
             };
 
-            // Act
             form.loginBTN_Click(null, EventArgs.Empty);
 
-            // Assert
+            Assert.True(messageBoxShown, "Введите номер и пароль");
+        }
+
+        [Fact]
+        public void LoginBTN_Click_EmptyPassword_ShowsErrorMessage()
+        {
+            LoginForm form = new LoginForm();
+            form.textBox1 = new TextBox();
+            form.textBox2 = new TextBox();
+            form.textBox1.Text = "login";
+            form.textBox2.Text = "";
+
+            bool messageBoxShown = false;
+            string messageBoxText = "";
+
+            form.MessageBoxShow = (text) =>
+            {
+                messageBoxShown = true;
+                messageBoxText = text;
+            };
+
+            form.loginBTN_Click(null, EventArgs.Empty);
             Assert.True(messageBoxShown, "Введите номер и пароль");
         }
 
